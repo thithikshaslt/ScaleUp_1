a84b46fc9993af937699f15cafa8e3675cbe8ae6
Thithiksha <thithiksha.slt@gmail.com>
2025-01-25 22:43:34 +0530

    Update urls.py

--- ScaleUp/urls.py
+++ ScaleUp/urls.py
@@ -5,7 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('info.urls')),
-    path('info/', include('info.urls')),
     path('api/', include('apis.urls')),
     path('accounts/login/',
          auth_views.LoginView.as_view(template_name='info/login.html'), name='login'),
